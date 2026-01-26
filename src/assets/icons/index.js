609d8752ed184b26ecc4c7c0d5c5c11d522b2945
bitna0a <qlcsk0147@naver.com>
2026-01-26 23:29:01 +0900

    ✨[feat]: Input, 관계 뱃지 컴포넌트 구현

---
+++ src/assets/icons/index.js
@@ -0,0 +1,21 @@
+import ArrowDownIcon from './ic-arrow-down.svg?react';
+import ArrowLeftIcon from './ic-arrow-left.svg?react';
+import ArrowRightIcon from './ic-arrow-right.svg?react';
+import CloseIcon from './ic-close.svg?react';
+import DeletedIcon from './ic-deleted.svg?react';
+import EditIcon from './ic-edit.svg?react';
+import ImojiIcon from './ic-imoji.svg?react';
+import PlusIcon from './ic-plus.svg?react';
+import ShareIcon from './ic-share.svg?react';
+
+export {
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  CloseIcon,
+  DeletedIcon,
+  EditIcon,
+  ImojiIcon,
+  PlusIcon,
+  ShareIcon,
+};
