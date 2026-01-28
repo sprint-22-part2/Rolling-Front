2846daa36f01a59eb51cf0669dba44c0f5a26eee
Songhyun <ziy1027@naver.com>
2026-01-28 13:28:11 +0900

    충돌 해결 (라우터, 아이콘)

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,29 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import Layout from '@/components/Layout';
 import RollingListPage from '@/pages/RollingListPage';
 import PostListPage from '@/pages/PostListPage';
-=======
 import ButtonPreview from '../pages/ButtonPreview/ButtonPreview';
->>>>>>> upstream/develop
 import ProfilePage from '@/pages/ProfilePage';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Layout />}>
           <Route index element={<div>Home Page</div>} />
           <Route path="/list" element={<RollingListPage />} />
           <Route path="/post" element={<PostListPage theme="blue" />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/preview/button" element={<ButtonPreview />} />
         </Route>
-=======
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route path="/preview/button" element={<ButtonPreview />} />
-        <Route path="/profile" element={<ProfilePage />} />
->>>>>>> upstream/develop
       </Routes>
     </BrowserRouter>
   );
