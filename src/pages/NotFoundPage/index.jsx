3797f56a29244dbd8d912dbc65aba6c0a6215a3a
GENIE\김지니상 <hgenie0604@gmail.com>
2026-02-04 16:44:22 +0900

    🐛[fix]: 에러페이지 css 정리 및 콜백함수 사용하도록 변경

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,11 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '@/components/common/Button';
 import styles from './index.module.css';
+import { useCallback } from 'react';
 
 import { AirplaneIcon } from '@/assets/icons';
 
 function NotFoundPage() {
   const navigate = useNavigate();
+
+  const handleGoHome = useCallback(() => {
+    navigate('/');
+  }, [navigate]);
 
   return (
     <div className={styles.container}>
@@ -13,11 +18,7 @@ function NotFoundPage() {
         <AirplaneIcon className={styles.airplane} />
         <p className={styles.desc}>접근 불가한 페이지 입니다</p>
 
-        <Button
-          size="sizeMd"
-          className={styles.homeBtn}
-          onClick={() => navigate('/')}
-        >
+        <Button size="sizeMd" className={styles.homeBtn} onClick={handleGoHome}>
           홈으로 돌아가기
         </Button>
       </div>
